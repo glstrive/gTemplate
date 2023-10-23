152f5f8a4451b4e25030d98d438749e508ca0766
gonglong <gongl@troy.cn>
2023-10-23 23:04:36 +0800

    fix: 修改bin命令

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,15 @@
+#!/usr/bin/env node
+
 /*
  * @Author: gonglong
  * @Date: 2023-10-17 09:55:16
  * @LastEditors: gonglong gongl@troy.cn
- * @LastEditTime: 2023-10-23 09:37:38
+ * @LastEditTime: 2023-10-23 22:58:45
  * @Description: 
  * 
- * Copyright (c) 2023 by ${git_name}, All Rights Reserved. 
+ * Copyright (c) 2023 by , All Rights Reserved. 
  */
+
  const List  = require('../lib/list')
  const Edit  = require('../lib/edit')
 
