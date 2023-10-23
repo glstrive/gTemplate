e2d47dc514c2ce8335d495dfb981874f30c8924d
gonglong <gongl@troy.cn>
2023-10-23 10:04:27 +0800

    fix:修改目录及删除无用文件

--- bin/index.js
+++ bin/index.js
@@ -2,13 +2,13 @@
  * @Author: gonglong
  * @Date: 2023-10-17 09:55:16
  * @LastEditors: gonglong gongl@troy.cn
- * @LastEditTime: 2023-10-19 21:13:44
+ * @LastEditTime: 2023-10-23 09:37:38
  * @Description: 
  * 
  * Copyright (c) 2023 by ${git_name}, All Rights Reserved. 
  */
- const List  = require('./lib/list')
- const Edit  = require('./lib/edit')
+ const List  = require('../lib/list')
+ const Edit  = require('../lib/edit')
 
  function start(){
    new List();
