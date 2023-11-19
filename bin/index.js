c04b832db39cf697152153a01ebfd3bacede1269
gonglong <gongl@troy.cn>
2023-11-19 17:05:49 +0800

    feat: 增加util生成  增加自定义columns

--- bin/index.js
+++ bin/index.js
@@ -4,7 +4,7 @@
  * @Author: gonglong
  * @Date: 2023-10-17 09:55:16
  * @LastEditors: gonglong gongl@troy.cn
- * @LastEditTime: 2023-10-23 22:58:45
+ * @LastEditTime: 2023-11-19 16:09:43
  * @Description: 
  * 
  * Copyright (c) 2023 by , All Rights Reserved. 
@@ -12,9 +12,11 @@
 
  const List  = require('../lib/list')
  const Edit  = require('../lib/edit')
+ const Utils = require('../lib/createUtils')
 
  function start(){
    new List();
    new Edit();
+   new Utils();
  }
  start()
